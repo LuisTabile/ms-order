c3e2291c5bca44acb79bd028938a370987d51137
LuisTabile <luistabile@github.com>
2023-01-26 15:22:02 -0300

    feat: added patch http method

--- msorder/src/main/java/com/ms/order/controllers/OrderController.java
+++ msorder/src/main/java/com/ms/order/controllers/OrderController.java
@@ -1,26 +1,11 @@
 package com.ms.order.controllers;
 
-import com.ms.order.models.dto.OrderDto;
-import com.ms.order.models.entities.OrderEntity;
-import com.ms.order.services.OrderService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/pedidos")
 public class OrderController {
-
-    @Autowired
-    OrderService orderService;
-
-
-    @PostMapping(value = "/api/pedidos/")
-    public ResponseEntity<OrderDto> create(@RequestBody @Valid OrderDto request){
-        OrderDto request = orderService.create(request);
-    }
 }
