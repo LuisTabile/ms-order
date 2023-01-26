28f5e27c425fe0a521f1a0e549380d5ecbbc1ab2
LuisTabile <luistabile@github.com>
2023-01-26 13:27:41 -0300

    refactor: removed annotation allargs its not needed

--- msorder/src/main/java/com/ms/order/constant/ErrorCode.java
+++ msorder/src/main/java/com/ms/order/constant/ErrorCode.java
@@ -1,17 +1,16 @@
 package com.ms.order.constant;
-
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
 public enum ErrorCode {
-    //Erros comuns
     BAD_REQUEST("Solicitação inválida"),
     INVALID_PARAMETER("Paramêtro da solicitação inválido"),
     INTERNAL_SERVER_ERROR("Erro interno do servidor"),
-    //Erros Not Found
-    ORDER_NOT_FOUND("Pedido não encontrado");
+    ORDER_NOT_FOUND("Pedido não encontrado"),
+    ITEM_NOT_FOUND("Item não encontrado");
+    @Getter
     private final String message;
+
+    ErrorCode(String message){
+        this.message = message;
     }
+}
