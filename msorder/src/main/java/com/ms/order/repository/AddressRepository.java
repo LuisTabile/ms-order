1d62b781fe49e0a4a0b311e79d95a11fd4d51e76
LuisTabile <luistabile@github.com>
2023-01-26 13:54:37 -0300

    feat: added address repository

--- msorder/src/main/java/com/ms/order/repository/AddressRepository.java
+++ msorder/src/main/java/com/ms/order/repository/AddressRepository.java
@@ -1,8 +1,10 @@
 package com.ms.order.repository;
 
+import com.ms.order.models.entities.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository extends JpaRepository<AddressRepository, Long> {
+public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
+    AddressEntity findByCep(String cep);
 }
