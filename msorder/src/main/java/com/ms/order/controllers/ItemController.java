8f71bbe6d3fed3ee63dbbd12ff0d16388002a9d1
LuisTabile <luistabile@github.com>
2023-01-27 12:54:19 -0300

    fix: fixed variable name

--- msorder/src/main/java/com/ms/order/controllers/ItemController.java
+++ msorder/src/main/java/com/ms/order/controllers/ItemController.java
@@ -6,6 +6,7 @@ import com.ms.order.services.ItemServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,7 +18,8 @@ public class ItemController {
     private final ItemServiceImpl itemService;
 
     @PatchMapping(value = "/{id}")
-    public ResponseEntity<ItemResponseDTO> update(@PathVariable("itemId") Long id, @Valid @RequestBody ItemRequestDTO itemRequestDTO) {
+    @Transactional
+    public ResponseEntity<ItemResponseDTO> update(@PathVariable("id") Long id, @Valid @RequestBody ItemRequestDTO itemRequestDTO) {
         ItemResponseDTO responseDTO = itemService.update(id, itemRequestDTO);
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
     }
