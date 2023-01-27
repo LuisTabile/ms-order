6f12478d341a8a5c3fad068e044351bc875dd07b
LuisTabile <luistabile@github.com>
2023-01-27 13:27:21 -0300

    build: build mshistory

---
+++ mshistory/src/main/java/com/ms/history/config/CORSConfig.java
@@ -0,0 +1,22 @@
+package com.ms.history.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+
+    private static final String ALLOWED_ORIGIN = "https://editor.swagger.io";
+    private static final String ALLOWED_METHODS = "GET, POST, PUT, PATCH, DELETE";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGIN)
+                .allowedMethods(ALLOWED_METHODS);
+    }
+}
+
