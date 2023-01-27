6d1923b3b02f0153b5087c5204fd7b17c6b0410e
LuisTabile <luistabile@github.com>
2023-01-27 00:42:31 -0300

    feat: added item controller

---
+++ msorder/src/main/java/com/ms/order/controllers/ItemController.java
@@ -0,0 +1,24 @@
+package com.ms.order.controllers;
+
+import com.ms.order.models.dto.request.ItemRequestDTO;
+import com.ms.order.models.dto.response.ItemResponseDTO;
+import com.ms.order.services.ItemServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/itens")
+public class ItemController {
+    private final ItemServiceImpl itemService;
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<ItemResponseDTO> update(@PathVariable("itemId") Long id, @Valid @RequestBody ItemRequestDTO itemRequestDTO) {
+        ItemResponseDTO responseDTO = itemService.update(id, itemRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+}
